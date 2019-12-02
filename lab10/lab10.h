3f0609075464dc55ce8a2b7a70fe450c466e16ad
mehmetkuckgtu <kucukmehmet197@gmail.com>
2019-12-02 23:26:03 +0300

    Update lab10.h

--- lab10/lab10.h
+++ lab10/lab10.h
@@ -17,4 +17,5 @@ typedef struct
 void labOn();
 void read_list(const char *file_name,food_t *food1,food_t *food2,food_t *food3,food_t *food4,food_t *food5,food_t *food6 );
 double safe_to_eat (food_t current_food);
+food_t get_params(void);
 #endif
